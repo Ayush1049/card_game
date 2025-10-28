a5f959d0d2766c3a29d77389bc622d93ffab4460
Ayush1049 <ayushrao595@gmail.com>
2025-10-28 21:39:24 +0530

    setup_of_teen-patti

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const PORT = 5000;
+const cors = require("cors");
+app.use(cors());
+
+
+app.get("/", (req, res) => {
+  res.send("Hello from Backend!");
+});
+
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
