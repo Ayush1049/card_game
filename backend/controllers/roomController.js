8c8baebbb7d8269c0ad7f5b89f78c9bdded9af99
Ayush1049 <ayushrao595@gmail.com>
2025-11-02 13:54:01 +0530

    joinRoom Controller and schema of room

--- backend/controllers/roomController.js
+++ backend/controllers/roomController.js
@@ -13,20 +13,27 @@ export const createRoom = asyncHandler(async(req,res)=>{
     console.log(username);
     const randomDigits = Math.floor(10000 + Math.random() * 90000);
     const roomId = `${username}${randomDigits}`;
-    console.log(roomId);
     const room = await prisma.room.create({
         data: {
             id: roomId,
             username:username,
             count_players: count_players
-        },
+        }
     });
-    if(room)
+    const game = await prisma.game.create({
+        data: {
+            room_id:roomId,
+            count_players: count_players,
+            players:[username]
+        }
+    });
+    if(game && room)
     {
         res.status(200).json({
-            id:room.id,
-            count_players:room.count_players,
-            status:"Room created successfully",
+            roomId:game.room_id,
+            count_players: game.count_players,
+            players:game.players,
+            status:`${username} joined the room ${roomId}`,
         })
     }
     else{
@@ -39,5 +46,44 @@ export const joinRoom = asyncHandler(async(req,res)=>{
     const { roomId } = req.params;
     console.log(roomId);
     const username = req.session.username;
-    res.json({ message: `${username} joined room ${roomId}`});
-})
+    //Appendig user each time when they join room via roomId
+    const gameRecord = await prisma.game.findUnique({ where: { room_id:roomId } });
+    //Checking whether room exist or not
+    if(gameRecord==false)
+    {
+        res.status(400);
+        throw new Error("Room doesn't exist");
+    };
+    // Check if room already full
+    if (gameRecord.players.length >= gameRecord.count_players) {
+        res.status(400);
+        throw new Error("Room is already full");
+    };
+    //Checking if user is already in the room
+    if (gameRecord.players.includes(username)) {
+        res.status(400);
+        throw new Error("User already in this room");
+    };
+    const updatedgame = await prisma.game.update({
+            where:{room_id:roomId},
+            data:{
+            players:{
+                push:[username]
+            }
+        }
+    });
+    if(updatedgame)
+    {
+        res.status(200).json({
+            roomId:updatedgame.room_id,
+            count_players:updatedgame.count_players,
+            players:updatedgame.players,
+            status:`${username} joined the room ${roomId} successfully`
+        });
+    }
+    else
+    {
+        res.status(400);
+        throw new Error(`${username} failed to join the room`);
+    };
+});
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { loginUser, registerUser, getAllUsers } from "../controllers/userController.js";
+import { loginUser, registerUser, getAllUsers} from "../controllers/userController.js";
 
 const router = express.Router();
 
